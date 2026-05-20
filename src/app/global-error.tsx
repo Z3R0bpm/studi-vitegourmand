be9b2bae9d1f6b6c846f4a449d146c3faec2e85e
Z3R0bpm <nochkillgames@gmail.com>
2026-05-20 23:40:25 +0200

    [ADD] Push prod

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,21 +1,12 @@
 "use client"
-import { Geist, Geist_Mono } from "next/font/google"
+import { GeistMono } from "geist/font/mono"
+import { GeistSans } from "geist/font/sans"
 import styles from "./styles/unexpected.module.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export default function GlobalError() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} ${styles.page}`}>
+      className={`${GeistSans.className} ${GeistMono.className} ${styles.page}`}>
       <h1 className={styles.notFound}>ERREUR</h1>
       <p>Une erreur est survenue. Veuillez réessayer plus tard.</p>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
+import { GeistMono } from "geist/font/mono"
+import { GeistSans } from "geist/font/sans"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
-import { Geist, Geist_Mono } from "next/font/google"
 import { ThemeHandler } from "./components/themeHandler"
 import { ThemeToggle } from "./components/ThemeToggle"
 import { CartProvider } from "./context/CartContext"
 import "./styles/globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Vite & Gourmand",
@@ -28,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${GeistSans.className} ${GeistMono.className}`}>
         <ThemeProvider
           attribute="data-theme"
           defaultTheme="system"
